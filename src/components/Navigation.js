7997cef08eb6c49d8618f2a51385493551eb4c50
vrydi <57484019+vrydi@users.noreply.github.com>
2022-03-04 18:31:24 +0100

    ask for notifications

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,41 @@
-import {Link} from "react-router-dom";
-import {Container, Nav, Navbar} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import { askForNotificationPermission } from "../serviceWorkerRegistration";
 
 export function NavbarBase() {
-
-    return <>
-        <Navbar collapseOnSelect expand='lg' fixed={'top'} bg={'dark'} variant={'dark'} className={'shadow p-2'}>
-            <Container>
-                <Navbar.Brand>
-                    Uno
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbar"/>
-                <Navbar.Collapse id="navbar">
-                    <Nav className="d-flex justify-content-center text-center w-100">
-                        <Nav.Item><Link to={"/"} className={'nav-link'}>Home</Link></Nav.Item>
-                        <Nav.Item><Link to={"/game"} className={'nav-link'}>Game</Link></Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        <div style={{height: "3.5em"}}/>
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        fixed={"top"}
+        bg={"dark"}
+        variant={"dark"}
+        className={"shadow p-2"}
+      >
+        <Container>
+          <Navbar.Brand>Uno</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar" />
+          <Navbar.Collapse id="navbar">
+            <Nav className="d-flex justify-content-center text-center w-100">
+              <Nav.Item>
+                <Link to={"/"} className={"nav-link"}>
+                  Home
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link to={"/game"} className={"nav-link"}>
+                  Game
+                </Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+          <Button onClick={() => askForNotificationPermission()}>
+            Send notifications
+          </Button>
+        </Container>
+      </Navbar>
+      <div style={{ height: "3.5em" }} />
     </>
+  );
 }
